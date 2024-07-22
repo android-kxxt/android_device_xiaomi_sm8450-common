62e680852251fe571c995df1b871a51510277252
kxxt <rsworktech@outlook.com>
2024-07-22 10:28:32 +0800

    sm8450-common: build virtualization binaries
    Change-Id: Iba5e28cac71c37ae5f7b2248ca66e5e84b333201

--- common.mk
+++ common.mk
@@ -513,6 +513,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Virtualization
+$(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
+PRODUCT_PACKAGES += \
+    vm \
+    virtualizationservice \
+    virtmgr
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
